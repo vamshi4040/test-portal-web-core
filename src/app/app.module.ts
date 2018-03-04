3d458639bfea1b738ef96e8022664322b151dbf8
vamshi4040 <upender785@gmail.com>
2018-03-04 20:34:16 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { routing } from './app.routing';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { ShowErrorsComponent } from './validators/show-errors.component';
+
+import { TelephoneNumberFormatValidatorDirective } from './validators/telephone-number-format-validator.directive';
+import { TelephoneNumbersValidatorDirective } from './validators/telephone-numbers-validator.directive';
+import { UniqueNameValidatorDirective } from './validators/name-unique-validator.directive';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ShowErrorsComponent,
+    TelephoneNumberFormatValidatorDirective,
+    TelephoneNumbersValidatorDirective,
+    UniqueNameValidatorDirective
   ],
   imports: [
     BrowserModule,
-      routing,
+    FormsModule,
+    routing,
     HttpClientModule
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,48 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Router} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {NgForm} from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
- data:any;
+  data: any;
   constructor(
-  	private router: Router,
+    private router: Router,
     private http: HttpClient
-  ) { }
+  ) {}
+
+  private count = 1;
+
+  phoneNumberIds: number[] = [1];
+
+  @ViewChild('myForm')
+  private myForm: NgForm;
+
+  printMyForm() {
+    console.log(this.myForm);
+  }
+
+  register(myForm: NgForm) {
+    console.log('Registration successful.');
+    console.log(myForm.value);
+  }
+
 
   ngOnInit() {
-      alert("OKKKKKKKKKKKKK");
   }
 
- validateUser() {
- 	//this.router.navigate(['sub-test']);
-    this.http.get('/test-portal-web/login').subscribe(data =>{
+
+  validateUser() {
+    console.log(this.data);
+    console.log(this.myForm.value);
+    //this.router.navigate(['sub-test']);
+    this.http.get('/test-portal-web/register').subscribe(data => {
       console.log(data);
-        this.data =data;
+      this.data = data;
     });
   }
- }
+}
 
